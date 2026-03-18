4d9d9bec3cb943b900e0b9923c0885f8ec2e388b
Santiago Pazmino <santiago.pazmino@isst.fraunhofer.de>
2026-03-18 11:58:49 +0100

    - Example Fhir Store Chart

--- src/main/java/de/fraunhofer/isst/health/store/spring/config/StoreVariablesConfig.java
+++ src/main/java/de/fraunhofer/isst/health/store/spring/config/StoreVariablesConfig.java
@@ -1,5 +1,6 @@
 package de.fraunhofer.isst.health.store.spring.config;
 
+import dev.dsf.bpe.v1.documentation.ProcessDocumentation;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -8,19 +9,37 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackages = "de.fraunhofer.isst.health.store")
 public class StoreVariablesConfig
 {
-
+	@ProcessDocumentation(required = true,processNames = {
+			"datamanagementuniteu_storeControllerCreate" },
+			description = "Base URL the store controllers share", example = "http://dmu-fhir-store")
 	@Value("${eu.datamanagementunit.store.hostname:#{null}}")
 	private String storeHostname;
 
+	@ProcessDocumentation(required = true,processNames = {
+			"datamanagementuniteu_storeControllerCreate" },
+			description = "Base URL of the git repository that holds the Kubernetes Helm Charts",
+			example = "https://github/dmu-argocd-charts.git")
 	@Value("${eu.datamanagementunit.store.git.url:#{null}}")
 	private String gitUrl;
 
+	@ProcessDocumentation(required = true,processNames = {
+			"datamanagementuniteu_storeControllerCreate" },
+			description = "Git Branch that should be pulled",
+			example = "main")
 	@Value("${eu.datamanagementunit.store.git.branch:#{null}}")
 	private String gitBranch;
 
+	@ProcessDocumentation(required = true,processNames = {
+			"datamanagementuniteu_storeControllerCreate" },
+			description = "Git User with access to the repository",
+			example = "main")
 	@Value("${eu.datamanagementunit.store.git.username:#{null}}")
 	private String gitUsername;
 
+	@ProcessDocumentation(required = true,processNames = {
+			"datamanagementuniteu_storeControllerCreate" },
+			description = "Git User password",
+			example = "main")
 	@Value("${eu.datamanagementunit.store.git.credentials:#{null}}")
 	private String gitCredentials;
 
