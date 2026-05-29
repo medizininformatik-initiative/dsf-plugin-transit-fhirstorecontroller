2eb83787a3b179b92bcfe7d7f0ffbe6a7a79d7de
Santiago Pazmino <santiago.pazmino@isst.fraunhofer.de>
2026-05-29 16:10:31 +0200

    - Fix Target DMS

--- src/main/java/de/fraunhofer/isst/health/store/service/create/PrepareStoreCreation.java
+++ src/main/java/de/fraunhofer/isst/health/store/service/create/PrepareStoreCreation.java
@@ -27,7 +27,7 @@ public class PrepareStoreCreation implements ServiceTask
 		String bussinessKey = getBussinessKey(task, api);
 		variables.setString(StoreControllerConstants.BPMN_EXECUTION_VARIABLE_BUSSINESS_KEY, bussinessKey);
 
-		String consortiumIdentifier = "Parent_Organization";
+		String consortiumIdentifier = "medizininformatik-initiative.de";
         //String consortiumIdentifier = variables
 		// .getString(StoreControllerConstants.BPMN_EXECUTION_VARIABLE_CONSORTIUM_IDENTIFIER);
 
@@ -65,7 +65,7 @@ public class PrepareStoreCreation implements ServiceTask
 	{
 		Identifier parentIdentifier = NamingSystems.OrganizationIdentifier.withValue(consortiumIdentifier);
 		Coding role = new Coding().setSystem(ConstantsBase.CODESYSTEM_DSF_ORGANIZATION_ROLE)
-				.setCode(ConstantsBase.CODESYSTEM_DSF_ORGANIZATION_ROLE_VALUE_DIC);
+				.setCode(ConstantsBase.CODESYSTEM_DSF_ORGANIZATION_ROLE_VALUE_DMS);
 		return api.getEndpointProvider().getEndpoint(parentIdentifier, dicIdentifier, role)
 				.orElseThrow(() -> new RuntimeException(
 						"Could not find default endpoint of organization '" + dicIdentifier.getValue() + "'"));
