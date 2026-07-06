a8c8809add3527afbdddcbf8cf3ee3c2a9ee6f28
Santiago Pazmino <santiago.pazmino@isst.fraunhofer.de>
2026-07-06 17:50:10 +0200

    - V1.0.0.3

--- src/main/java/de/fraunhofer/isst/health/store/service/create/PrepareStoreCreation.java
+++ src/main/java/de/fraunhofer/isst/health/store/service/create/PrepareStoreCreation.java
@@ -6,8 +6,6 @@ import de.medizininformatik_initiative.processes.common.util.ConstantsBase;
 import dev.dsf.bpe.v2.ProcessPluginApi;
 import dev.dsf.bpe.v2.activity.ServiceTask;
 import dev.dsf.bpe.v2.constants.CodeSystems;
-import dev.dsf.bpe.v2.constants.NamingSystems;
-import dev.dsf.bpe.v2.variables.Target;
 import dev.dsf.bpe.v2.variables.Variables;
 import org.hl7.fhir.r4.model.*;
 import org.slf4j.Logger;
@@ -36,28 +34,17 @@ public class PrepareStoreCreation implements ServiceTask
 
 		variables.setBoolean(StoreControllerConstants.BPMN_EXECUTION_KUBERNETES, storeVariablesConfig.isKubernetes());
 
-		//String consortiumIdentifier = "medizininformatik-initiative.de";
-		String consortiumIdentifier = "Parent_Organization";
-        //String consortiumIdentifier = variables
-		// .getString(StoreControllerConstants.BPMN_EXECUTION_VARIABLE_CONSORTIUM_IDENTIFIER);
-
-
-		Identifier dicIdentifier = getDicOrganizationIdentifier(task);
-		Endpoint dicEndpoint = getDicEndpoint(api, consortiumIdentifier, dicIdentifier);
-
-		Target dicTarget = createTarget(variables, dicIdentifier, dicEndpoint);
-		variables.setTarget(dicTarget);
+		//DMS Target for Store controller
+		variables.setTarget(
+				variables.createTarget(api.getOrganizationProvider().getLocalOrganizationIdentifierValue().get(),
+						api.getEndpointProvider().getLocalEndpointIdentifierValue().get(),
+						api.getEndpointProvider().getLocalEndpointAddress()));
 
 		logger.info(
 				"Starting store creation of approved data sharing project [project-identifier: {}; task-id: {}]",
 				projectIdentifier,
 				task.getId());
 
-	}
-
-	private Identifier getDicOrganizationIdentifier(Task task)
-	{
-		return task.getRequester().getIdentifier();
 	}
 
 	private String getProjectIdentifier(Task task)
@@ -69,30 +56,6 @@ public class PrepareStoreCreation implements ServiceTask
 				.filter(i -> ConstantsBase.NAMINGSYSTEM_MII_PROJECT_IDENTIFIER.equals(i.getSystem()))
 				.map(Identifier::getValue).map(String::trim).findFirst().orElseThrow(() -> new RuntimeException(
 						"No project-identifier present in task with id '" + task.getId() + "'"));
-	}
-
-	private Endpoint getDicEndpoint(ProcessPluginApi api, String consortiumIdentifier, Identifier dicIdentifier)
-	{
-		Identifier parentIdentifier = NamingSystems.OrganizationIdentifier.withValue(consortiumIdentifier);
-		Coding role = new Coding().setSystem(ConstantsBase.CODESYSTEM_DSF_ORGANIZATION_ROLE)
-				.setCode(ConstantsBase.CODESYSTEM_DSF_ORGANIZATION_ROLE_VALUE_DMS);
-		return api.getEndpointProvider().getEndpoint(parentIdentifier, dicIdentifier, role)
-				.orElseThrow(() -> new RuntimeException(
-						"Could not find default endpoint of organization '" + dicIdentifier.getValue() + "'"));
-	}
-
-	private Target createTarget(Variables variables, Identifier dicIdentifier, Endpoint dicEndpoint)
-	{
-		String dicEndpointIdentifier = extractEndpointIdentifier(dicEndpoint);
-		return variables.createTarget(dicIdentifier.getValue(), dicEndpointIdentifier, dicEndpoint.getAddress());
-	}
-
-	private String extractEndpointIdentifier(Endpoint endpoint)
-	{
-		return endpoint.getIdentifier().stream().filter(i -> NamingSystems.EndpointIdentifier.SID.equals(i.getSystem()))
-				.map(Identifier::getValue).findFirst()
-				.orElseThrow(() -> new RuntimeException("Endpoint with id '" + endpoint.getId()
-						+ "' is missing identifier with system '" + NamingSystems.EndpointIdentifier.SID + "'"));
 	}
 
 	private String getBussinessKey(Task task, ProcessPluginApi api)
